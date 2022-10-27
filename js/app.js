0b52b42b4725053b203d1d363615f513ae8edc2e
Tokway <linktokway@gmail.com>
2022-10-27 23:31:29 +0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,27 @@
+window.addEventListener('scroll', e => {
+	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
+})
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+	wrapper: '.wrapper',
+	content: '.content'
+})
+
+
+$(function(){
+	var shrinkHeader = 600;
+	 $(window).scroll(function() {
+	   var scroll = getCurrentScroll();
+		 if ( scroll >= shrinkHeader ) {
+			  $('.header').addClass('shrink');
+		   }
+		   else {
+			   $('.header').removeClass('shrink');
+		   }
+	 });
+   function getCurrentScroll() {
+	   return window.pageYOffset || document.documentElement.scrollTop;
+	   }
+   });
+
+
